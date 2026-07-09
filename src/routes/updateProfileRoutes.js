6d8450eea33a2a09e4e28dccb4b8ea65b760965d
Amardeep <kamardeep418@gmail.com>
2026-07-09 14:07:33 +0530

    added get profile image api for user

--- src/routes/updateProfileRoutes.js
+++ src/routes/updateProfileRoutes.js
@@ -3,21 +3,57 @@ const router = express.Router();
 
 const { authMiddleware } = require("../middlewares/authMiddleware");
 const { userMiddleware } = require("../middlewares/userMiddleware");
+const { candidateUpload } = require("../middlewares/uploadMiddleware");
 const updateProfileController = require("../controllers/updateProfileController");
 
 //  Get profile (prefill form)
-router.get("/", authMiddleware,userMiddleware, updateProfileController.getProfile);
+router.get(
+  "/",
+  authMiddleware,
+  userMiddleware,
+  updateProfileController.getProfile,
+);
 
 //  Update profile
-router.put("/", authMiddleware, userMiddleware, updateProfileController.updateProfile);
+router.put(
+  "/",
+  authMiddleware,
+  userMiddleware,
+  updateProfileController.updateProfile,
+);
 
 // update password
-router.put("/change-password", authMiddleware,userMiddleware, updateProfileController.changePassword);
+router.put(
+  "/change-password",
+  authMiddleware,
+  userMiddleware,
+  updateProfileController.changePassword,
+);
 
+// update profile image
+router.put(
+  "/update_profile_image",
+  authMiddleware,
+  userMiddleware,
+  candidateUpload,
+  updateProfileController.updateProfileImage,
+);
+
+// update profile image
+router.get(
+  "/get_profile_image",
+  authMiddleware,
+  userMiddleware,
+  updateProfileController.getProfileImage,
+);
 
 // Mobile Apis=============================================================================================================================
 
-
-router.put("/update_user",authMiddleware,userMiddleware,updateProfileController.appUpdateUserProfileApp);
+router.put(
+  "/update_user",
+  authMiddleware,
+  userMiddleware,
+  updateProfileController.appUpdateUserProfileApp,
+);
 
 module.exports = router;
